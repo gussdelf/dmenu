27ff2d04bfeff9332b4452715fea56618a373e29
Gustavo Freitas <flovatsug@gmail.com>
2021-12-13 15:05:45 -0300

    First commit

---
+++ colors/city-lights.h
@@ -0,0 +1,9 @@
+static const char *colors[SchemeLast][2] = {
+    /*     fg         bg       */
+    [SchemeNorm] = {"#7FA0B7", "#1D252C"},
+    [SchemeSel] = {"#000000", "#E27E8D"},
+    [SchemeSelHighlight] = {"#8BD49C", "#000000"},
+    [SchemeNormHighlight] = {"#8BD49C", "#000000"},
+    [SchemeOut] = {"#1D252C", "#70E1E8"},
+    [SchemeMid] = {"#7FA0B7", "#000000"},
+};
