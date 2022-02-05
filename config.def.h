69be84dca8baeb9aebb3e3ab5f8147870c148356
Gustavo Freitas <gussdelf@gmail.com>
2022-02-05 18:10:52 -0300

    saci

--- config.def.h
+++ config.def.h
@@ -7,16 +7,16 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;       /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;     /* -c option; centers dmenu on screen */
-static int min_width = 600;  /* minimum width when centered */
-static int fuzzy = 1;        /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+static int topbar = 1;      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;    /* -c option; centers dmenu on screen */
+static int min_width = 600; /* minimum width when centered */
+static int fuzzy = 1; /* -F  option; if 0, dmenu doesn't use fuzzy matching */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"IosevkaNerdFont:pixelsize=13:antialias=true:autohint=true",
-	"JoyPixels:pixelsize=8:antialias=true:autohint=true"
-};
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+    "IosevkaNerdFont:pixelsize=13:antialias=true:autohint=true",
+    "JoyPixels:pixelsize=8:antialias=true:autohint=true"};
+static const char *prompt =
+    NULL; /* -p  option; prompt to the left of input field */
 
 /*
  * SELECT YOUR COLOR SCHEME
@@ -32,12 +32,13 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
  * 9. tomorrow-night.h
  * 10. city-lights.h
  */
-#include "colors/city-lights.h"
+#include "colors/gruvbox-dark.h"
 
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
-static unsigned int lines      = 0;
-static unsigned int lineheight = 22;  /* -h option; minimum height of a menu line */
-static unsigned int columns    = 0;
+static unsigned int lines = 0;
+static unsigned int lineheight =
+    22; /* -h option; minimum height of a menu line */
+static unsigned int columns = 0;
 
 /*
  * Characters not considered part of a word while deleting words
@@ -46,4 +47,4 @@ static unsigned int columns    = 0;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 0;  /* -bw option; to add border width */
+static unsigned int border_width = 0; /* -bw option; to add border width */
