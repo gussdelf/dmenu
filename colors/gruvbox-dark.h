c1f646e9dc449a6a305e727ca0929ff4e8c62aae
Gustavo Freitas <gussdelf@gmail.com>
2022-02-25 17:45:56 -0300

    af

--- colors/gruvbox-dark.h
+++ colors/gruvbox-dark.h
@@ -1,9 +1,9 @@
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#ebdbb2", "#282828" },
-	[SchemeSel] = { "#000000", "#b16286" },
-	[SchemeSelHighlight] = { "#689d6a", "#000000" },
-	[SchemeNormHighlight] = { "#689d6a", "#000000" },
-	[SchemeOut] = { "#ffffff", "#458588" },
-	[SchemeMid] = { "#ebdbb2", "#1c1f24" },
+    /*     fg         bg       */
+    [SchemeNorm] = {"#ebdbb2", "#1d2021"},
+    [SchemeSel] = {"#000000", "#fb4934"},
+    [SchemeSelHighlight] = {"#689d6a", "#000000"},
+    [SchemeNormHighlight] = {"#689d6a", "#000000"},
+    [SchemeOut] = {"#ffffff", "#458588"},
+    [SchemeMid] = {"#ebdbb2", "#1c1f24"},
 };
